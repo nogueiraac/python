2cec2b1472244bfad4c0a001c38f6febaed35b87
nogueiraac <240700ac@gmail.com>
2022-01-27 21:55:06 -0300

    Updating REAME, resolving challenge 12.

---
+++ sequencial_structure/challenge12.py
@@ -0,0 +1,5 @@
+altura = float(input("Insira a altura: "))
+
+peso_ideal = (72.7*altura) - 58
+
+print("Peso ideal seria: ", peso_ideal)
--- sequencial_structure/challenge13.py
+++ sequencial_structure/challenge13.py
@@ -2,7 +2,7 @@ altura = float(input("Insira a altura: "))
 sexo = input("Informe o sexo: ")
 
 if sexo == "homem":
-    peso_ideal = (72*altura) - 58
+    peso_ideal = (72.7*altura) - 58
 elif sexo == "mulher":
     peso_ideal = (62.1*altura) - 44.7
 
